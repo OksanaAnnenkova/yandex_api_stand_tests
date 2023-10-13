2c2ab9b63faa2e6e14e11bc4a06506f59ff603ae
OksanaAnnenkova <147560121+OksanaAnnenkova@users.noreply.github.com>
2023-10-13 11:38:19 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,7 @@
+URL_SERVICE = "https://809d34e5-5547-419a-b795-34c17adb79b6.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
